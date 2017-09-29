445ed345d4eb8e18d0e2812f265f1f82cdd44336
acwajega <31436401+acwajega@users.noreply.github.com>
2017-09-29 19:22:29 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 module.exports = {
     'connection_parameters' : {
   host: 'localhost',
-  user: 'root',	
-  password: 'matilda',
-  database: 'esente_db'}
+  user: 'wacsoft',	
+  password: 'matilda123',
+  database: 'eSente_db'}
  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
